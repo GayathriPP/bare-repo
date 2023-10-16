d8a0b36900b669cc51cd9c3479294fa02fb424e8
user <example@gmail.com>
2023-10-16 13:02:04 +0000

    added new project

---
+++ src/main/java/com/example/barerepo/BareRepoApplication.java
@@ -0,0 +1,13 @@
+package com.example.barerepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BareRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BareRepoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/barerepo/BareRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.barerepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BareRepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
